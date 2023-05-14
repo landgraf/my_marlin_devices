fbd7e0948d09d1de1b5922a869d4471bc877a9fe
Pavel Zhukov <pavel@zhukoff.net>
2023-05-14 11:06:43 +0200

    Update bed size and home point

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1690,8 +1690,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 1220 // 200
-#define Y_BED_SIZE 2440 // 200
+#define X_BED_SIZE 1250 // 200
+#define Y_BED_SIZE 2500 // 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1699,7 +1699,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 80
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
